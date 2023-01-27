e054e17fe36bf15a5271b66140ad5e8b804c9897
Pablo <47283527+PabloSR06@users.noreply.github.com>
2023-01-27 19:34:46 +0000

    WIP: Initial project

---
+++ Data/QnAMakerService.cs
@@ -0,0 +1,102 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace TrimQnaMaker.Data
+{
+    internal class QnAMakerService
+    {
+        public Task<List<QnaDocumentMin>> GetBadQna(KeysModel keys)
+        {
+            HttpClient client = GetHttpClient(keys.endpoint, keys.subscriptionId);
+            HttpResponseMessage response = client.GetAsync($"{keys.kbid}/Test/qna").Result;
+
+            QnaMin qna = new QnaMin();
+            List<QnaDocumentMin> badPairs = new List<QnaDocumentMin>();
+
+            if (response.IsSuccessStatusCode)
+            {
+                qna = JsonConvert.DeserializeObject<QnaMin>(response.Content.ReadAsStringAsync().Result);
+
+
+                if (qna != null)
+                {
+                    foreach (var pair in qna.QnaDocuments)
+                    {
+                        if (pair.Questions.Count >= keys.amount)
+                        {
+                            badPairs.Add(pair);
+                        }
+                    }
+                }
+                
+            }
+            
+            return Task.FromResult(badPairs);
+        }
+
+        public async Task<HttpStatusCode> UpgradeKB(List<QnaDocumentMin> qnas, KeysModel keys)
+        {
+            HttpClient client = GetHttpClient(keys.endpoint, keys.subscriptionId);
+
+            var request = new HttpRequestMessage(new HttpMethod("PATCH"), $"{keys.kbid}");
+
+            List<DeleteQuestions> delete = new List<DeleteQuestions>();
+
+            foreach (var x in qnas) {
+
+                if (x.hasChanged)
+                {
+                    var toDelete = new DeleteQuestions();
+
+                    toDelete.Id = x.Id;
+                    toDelete.Questions = new Metadata
+                    {
+                        delete = x.QuestionToDelete,
+                    };
+                    delete.Add(toDelete);
+                }
+            }
+
+            request.Content = new StringContent(JsonConvert.SerializeObject(delete), Encoding.UTF8, "application/json");
+
+            var response = await client.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception(string.Format("Update QNA failed '{0}'", response.ReasonPhrase));
+
+
+            return HttpStatusCode.Accepted;
+        }
+
+        public static HttpClient GetHttpClient(string endpoint, string subscription)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri($"{endpoint}/qnamaker/v4.0/knowledgebases/"),
+                Timeout = new TimeSpan(0, 2, 0)    // Standard two minute timeout on web service calls.
+            };
+            HttpRequestHeaders headers = client.DefaultRequestHeaders;
+
+
+            headers.Add("OData-MaxVersion", "4.0");
+            headers.Add("Ocp-Apim-Subscription-Key", $"{subscription}");
+            headers.Add("OData-Version", "4.0");
+            headers.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+    }
+
+   
+
+    
+}
---
+++ Data/Qna.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TrimQnaMaker.Data
+{
+    public partial class KeysModel
+    {
+        //Supported Cognitive Services endpoint
+        public string endpoint;
+        //Ocp-Apim-Subscription-Key
+        public string subscriptionId;
+        //Knowledgebase id 
+        public string kbid;
+        //Amount of questions to filter
+        public int amount;
+    }
+    public partial class QnaDocumentMin
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+        [JsonProperty("questions")]
+        public List<string> Questions { get; set; }
+
+        [JsonProperty("answer")]
+        public string Answer { get; set; }
+
+        public bool hasChanged { get; set; }
+        public List<string> QuestionToDelete { get; set; }
+
+    }
+
+    public partial class QnaMin
+    {
+        [JsonProperty("qnaDocuments")]
+        public List<QnaDocumentMin> QnaDocuments { get; set; }
+    }
+
+    public partial class DeleteQuestions
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("questions")]
+        public Metadata Questions { get; set; }
+    }
+    public partial class Metadata
+    {
+        [JsonProperty("delete")]
+        public List<string> delete { get; set; }
+
+
+    }
+
+}
